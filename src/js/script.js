9136b3d81f0f9342f82aedd40efbda59018d0874
Dmitriy Safronov <svsdima1995@mail.ru>
2020-09-23 15:04:08 +0300

    Сделал третью секцию в меню и слайдами, сделал четвёртую секцию, сделал пятую секцию с небольшой анимацией при наведении на нужный вариант

--- src/js/script.js
+++ src/js/script.js
@@ -25,17 +25,17 @@ $(document).ready(function(){
     });
 
       $('.carousel__mac').slick({
-        speed: 2000, /* Скорость перелистывания */
+        speed: 1500, /* Скорость перелистывания */
         // adaptiveHeight: true, /* Подстраивается под высоту картинки */
         // autoplay: true, /* Автопереключение слайдов */
         autoplaySpeed: 4000, /* Скорость автопереключения */
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        // arrows: false,
         dots: false,
         // dots:true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slick_bg_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slick_bg_right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slick_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slick_right.png"></button>',
         responsive: [
             {
                 breakpoint: 768,
